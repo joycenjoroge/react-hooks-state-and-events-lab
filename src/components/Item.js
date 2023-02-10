c123ffdb8c7672e5e23f0dd137cfab8a87e044d2
Joyce <joycenjoroge47@gmail.com>
2023-02-10 11:12:05 +0300

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [isOn, setIsOn] = useState(false);
+  const itemClass = isOn ?"in-cart":" "
+
+  function handleClick(){
+    setIsOn((isOn=>!isOn))
+   }
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{isOn? "Remove to Cart":"Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,33 @@
 import React from "react";
 import Item from "./Item";
+import { useState ,useEffect,useMemo} from "react";
 
 function ShoppingList({ items }) {
+ 
+  const [shoppingList, setShoppingList] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState();
+
+  useEffect(() => {
+    setShoppingList(items);
+  }, []);
+
+  function getFilteredList() {
+    if (!selectedCategory) {
+      return shoppingList;
+    }
+    return shoppingList.filter((item) => item.category === selectedCategory);
+  }
+
+  let filteredList =useMemo(getFilteredList,[selectedCategory,shoppingList]);
+
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+ }
+ 
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +35,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredList.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
